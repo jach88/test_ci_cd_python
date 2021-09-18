260897b70a9074e132f06624b912fb9d3c595eed
Christian Julca <jach88@hotmail.com>
2021-09-17 21:39:14 -0500

    Commit agregando mas test

--- controllers/actividades.py
+++ controllers/actividades.py
@@ -1,4 +1,15 @@
+
 from flask_restful import Resource, reqparse
+
+
+serializador = reqparse.RequestParser(bundle_errors=True)
+serializador.add_argument(
+    'actividadNombre',
+    required=True,
+    location='json',
+    help='Falta la actividadNombre',
+    type=str
+)
 
 class ActividadesController(Resource):
     def get(self):
@@ -16,3 +27,16 @@ class ActividadesController(Resource):
                 "actividadNombre":"Ir al cumpleaños"
             }]
         },201
+    
+    def post(self):
+        data:dict = serializador.parse_args()
+        #logica para registrar la actividad en la bd
+        actividadCreada ={
+            "actividadId":50,
+            "actividadNombre": data.get('actividadNombre')
+        }
+        return {
+            "message": "actividad creada exitosamente",
+            "content": actividadCreada            
+        },201
+
